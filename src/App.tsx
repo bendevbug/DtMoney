134dae7f783cf411baf33a517a28df6418b26423
Maysa <maysaraquel.ia@gmail.com>
2022-08-27 11:18:35 -0300

    docs: Atualizações com react-modal, criado o Modal e colocado na aplicação, adicionado as props a este componente dentro por meio do componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import { Dashboard } from "./components/dashboard";
 import { Header } from "./components/Header";
-import { Sumarry } from "./components/sumarry";
 import { GlobalStyle } from "./styles/global";
-
+import Modal from 'react-modal';
+import React from "react";
 
 export function App() {
+
+    Modal.setAppElement('#root');
+
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = React.useState(false);
+  
+    function newTransaction() {
+      setIsNewTransactionModalOpen(true);
+    }
+  
+    function closeModal() {
+      setIsNewTransactionModalOpen(false);
+    }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={newTransaction} />
       <Dashboard />
+       <Modal 
+      isOpen={isNewTransactionModalOpen}
+      onRequestClose={closeModal} >
+          <h2>hELLO ItsS Are YU LukinG fo</h2>
+       </Modal>
       <GlobalStyle />
     </>
   );
--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Sumarry } from "../sumarry";
+import { TransactionTable } from "../transactionsTable";
 import { Container } from "./style";
 
 export function Dashboard() {
     return(
         <Container>
             <Sumarry />
+            <TransactionTable />
         </Container>
     );
 }
