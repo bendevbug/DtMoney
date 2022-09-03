b6b4102ce54978daaf7a4a0d71e1ef8c1eb79238
Maysa <maysaraquel.ia@gmail.com>
2022-09-03 11:49:46 -0300

    docs: Realizado o método post da API, visualização d evalores na tabela de acordo com o que contém na api, transformação da data e tipo do dinheiro usado no país

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,7 +5,8 @@ import { RadioBox } from './style';
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 
 interface NewTransactionModalProps {
@@ -14,8 +15,23 @@ interface NewTransactionModalProps {
   }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState(0);
+    const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit')
+
+    function handleCreateNewTransaction(event: FormEvent) {
+        event.preventDefault();
+
+       const data = {
+          title,
+          value,
+          category,
+          type
+        };
+
+        api.post('/transactions', data)
+    }
 
     return(
         <Modal 
@@ -31,11 +47,19 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           <img src={closeImg} alt="Fechar modal"/>
         </button>
 
-        <Container>
+        <Container onSubmit={handleCreateNewTransaction}>
           <h2>Cadastrar transação</h2>
 
-          <input placeholder="Título"/>
-          <input placeholder="Valor" type="number"/>
+          <input placeholder="Título" 
+          value={title} 
+          onChange={event => setTitle(event.target.value)}
+          />
+
+          <input placeholder="Valor" 
+          type="number" 
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+          />
 
           <TransactionTypeContainer>
 
@@ -59,7 +83,10 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
           </TransactionTypeContainer>
 
-          <input placeholder="Categoria"/>
+          <input placeholder="Categoria"
+          value={category} 
+          onChange={event => setCategory(event.target.value)}
+          />
 
           <button type="submit">
             Cadastrar
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,61 @@
-import { createServer } from 'miragejs';
+import { request } from 'http';
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
 
 createServer({
+
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          tiype: 'deposit',
+          category: 'dev',
+          amount: 6000,
+          createdAt: new Date('2022-08-01 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Freelancer de website',
+          tiype: 'deposit',
+          category: 'dev',
+          amount: 450,
+          createdAt: new Date('2022-04-01 09:00:00')
+        },
+        {
+          id: 3,
+          title: 'Poupança',
+          tiype: 'deposit',
+          category: 'banco',
+          amount: 1000,
+          createdAt: new Date('2021-08-01 09:00:00')
+        }
+      ],
+    })
+  },
+
     routes() {
       this.namespace = 'api';
 
       this.get('/transactions', ()=> {
-        return [
-          {
-            id: 1,
-            title: 'transaction 1',
-            amount: 400,
-            type: 'deposit',
-            category: 'food',
-            createdAt: new Date(),
-          }
-        ]
+        return this.schema.all('transaction')
       })
+
+
+      this.post('/transactions', (schema, request) => {
+          const data = JSON.parse(request.requestBody)
+
+
+          return schema.create('transaction', data);
+      } )
     }
   })
 
